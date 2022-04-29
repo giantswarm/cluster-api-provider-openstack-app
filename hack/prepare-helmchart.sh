8e1f442b4a8d03cd3e38c411bf9261d50ac91b33
Mario Constanti <mario@constanti.de>
2022-04-29 10:57:00 +0200

    move capo into giantswarm namespace

--- hack/prepare-helmchart.sh
+++ hack/prepare-helmchart.sh
@@ -2,6 +2,13 @@
 
 # ${1} is the given application-name from make (cluster-api-provider-openstack)
 
+# let's rename everything to giantswarm namespace - it's much more easier to rename all occurences with sed than with kustomize - i've tried it :-)
+find config/kustomize/tmp/ -type f | while read f; do
+	sed -i 's/capo-system/giantswarm/g' ${f}
+done
+
+find config/kustomize/tmp -name ".*namespace.*" -delete
+
 # as we apply the crd via configmap, name must be stripped to be RFC 1123 conform
 # (lower case alphanumeric characters or '-', and must start and end with an alphanumeric character)
 find config/kustomize/tmp/ -name "apiextensions.k8s.io_v1_customresourcedefinition*" | while read f; do
