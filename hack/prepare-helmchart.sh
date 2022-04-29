53b61f30cb69df5668cebd7da2b6dd9fcb41afa2
Mario Constanti <mario@constanti.de>
2022-04-29 11:37:57 +0200

    change find param

--- hack/prepare-helmchart.sh
+++ hack/prepare-helmchart.sh
@@ -7,15 +7,15 @@ find config/kustomize/tmp/ -type f | while read f; do
 	sed -i 's/capo-system/giantswarm/g' ${f}
 done
 
-find config/kustomize/tmp -name ".*namespace.*" -delete
+find config/kustomize/tmp -regex ".*namespace.*" -delete
 
 # as we apply the crd via configmap, name must be stripped to be RFC 1123 conform
 # (lower case alphanumeric characters or '-', and must start and end with an alphanumeric character)
-find config/kustomize/tmp/ -name "apiextensions.k8s.io_v1_customresourcedefinition*" | while read f; do
+find config/kustomize/tmp/ -regex ".*apiextensions.k8s.io_v1_customresourcedefinition.*" | while read f; do
 	mv -v ${f} helm/${1}/crds/${f/config\/kustomize\/tmp\/apiextensions.k8s.io_v1_customresourcedefinition_/}
 done
 
-find helm/${1}/crds/ -name "*infrastructure.cluster.x-k8s.io.yaml" | while read f; do
+find helm/${1}/crds/ -regex ".*infrastructure.cluster.x-k8s.io.yaml" | while read f; do
 	mv -v ${f} ${f/infrastructure.cluster.x-k8s.io./}
 done
 
