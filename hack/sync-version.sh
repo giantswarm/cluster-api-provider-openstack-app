18df80c638c692207b06ef734c49d95d4b380cbf
Mario Constanti <github@constanti.de>
2022-05-12 19:42:08 +0200

    Marioc initial capo from upstream (#13)
    * delete existing capo manifest content
    
    As we are syncing upstream manifest let's drop the existing manifest
    files for now.
    
    * fetch upstream manifest
    
    fetch upstream capo manifest
    
    * make targets for all individual steps
    
    * adjust upstream capo manifest
    
    * apply some generic labels and annotations to make it
      giantswarm/app-catalog compatible
    * apply some kube-linter findings
    
    * change circleci config to get a app release ...
    
    ... by commiting to this repo
    
    * list all known objects and apply kustomize patches
    
    * don't apply helm annotations on crds
    
    * bump to capo 0.5.3
    
    * bump config annotation version
    
    * use quay.io giantswarm org image
    
    * only build manifest from release tag
    
    * add some notes about this repo
    
    * add findings from PR
    
    * revert circleci config change
    
    * move capo into giantswarm namespace
    
    * change find param
    
    * hope to fix some linux/mac compatibility issues
    
    * namespace replacement done via kustomize
    
    * format all files
    
    * template CRDs before setting as content
    
    * install crd later
    
    * rname crd folder
    
    * used file format from upstream
    
    * enable metrics port ...
    
    ... and re-enable app-build-suite executor
    
    * make us of git checkout -d
    
    * detach HEAD at named commit to make it possible to checkout by
      tag-name or branch name
    
    * remove executor
    
    * add comment about crd_webhook patch

---
+++ hack/sync-version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "${0}")"
+CAPO_SYNC_BRANCH=${1:-"main"}
+
+# create a temporary directory and checkout CAPO there
+TMPDIR=$(mktemp -d)
+pushd "${TMPDIR}"
+
+git clone https://github.com/kubernetes-sigs/cluster-api-provider-openstack.git
+pushd cluster-api-provider-openstack
+
+git checkout -d "${CAPO_SYNC_BRANCH}"
+
+# RELEASE_TAG and REGISTRY are only defined to have more unique strings to replace later via kustomize
+RELEASE_TAG="dev" REGISTRY="giantswarm/kaas" make release-manifests
+
+# remote cluster-api-provider-openstack from the stack
+popd
+
+# remote $TMPDIR from the stack
+popd
+
+# copy upstream generated release-manifests into origin
+cp -v "${TMPDIR}/cluster-api-provider-openstack/out/infrastructure-components.yaml" "../config/kustomize/input/"
