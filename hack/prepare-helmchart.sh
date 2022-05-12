e0189143f4d5002b90e0d12f5ffbcd80fd93909b
Mario Constanti <mario@constanti.de>
2022-05-12 07:30:33 +0200

    rname crd folder

--- hack/prepare-helmchart.sh
+++ hack/prepare-helmchart.sh
@@ -6,10 +6,10 @@
 # (lower case alphanumeric characters or '-', and must start and end with an alphanumeric character)
 find config/kustomize/tmp/ -regex ".*apiextensions.k8s.io_v1_customresourcedefinition.*" | while read f; do
 	filename=$(basename ${f})
-	mv -v config/kustomize/tmp/${filename} helm/${1}/crds/${filename/apiextensions.k8s.io_v1_customresourcedefinition_/}
+	mv -v config/kustomize/tmp/${filename} helm/${1}/files/${filename/apiextensions.k8s.io_v1_customresourcedefinition_/}
 done
 
-find helm/${1}/crds/ -regex ".*infrastructure.cluster.x-k8s.io.yaml" | while read f; do
+find helm/${1}/files/ -regex ".*infrastructure.cluster.x-k8s.io.yaml" | while read f; do
 	mv -v ${f} ${f/infrastructure.cluster.x-k8s.io./}
 done
 
